f21d411d97a8a2da26a46f46d1b3d5ecf7dc6f94
Samuel Lopez <sam@Samuels-MacBook-Air-8.local>
2024-02-08 16:09:04 -0800

    erm

--- module.py
+++ module.py
@@ -1,10 +1,1 @@
-from person import Person
-
-class Student(Person):
-    def __init__(self, name, age, year):
-        super().__init__(name, age)
-        self.year = year
-
-if __name__ == '__main__':
-    student1 = Student('John', 18, 'Freshman')
-    person1 = Person('Joe', '19')
+API_KEY = ""
